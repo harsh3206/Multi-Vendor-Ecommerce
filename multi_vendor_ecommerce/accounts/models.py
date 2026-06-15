27445f3c43aeea2a7dca26718271e90f142a816f
harsh solanki <solankiharsh7755@gmail.com>
2026-06-15 11:58:10 +0530

    Recover

---
+++ multi_vendor_ecommerce/accounts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin','Admin'),
+        ('vendor','Vendor'),
+        ('customer','Customer'),
+    )
+    role=models.CharField(max_length=10,choices=ROLE_CHOICES)
--- multi_vendor_ecommerce/build.sh
+++ multi_vendor_ecommerce/build.sh
@@ -5,6 +5,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# load data automatically (IMPORTANT)
-python manage.py loaddata data.json || true
