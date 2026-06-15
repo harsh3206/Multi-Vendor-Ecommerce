fada761f9e01fab92a7400a0bd07c6edb03ff5c5
harsh solanki <solankiharsh7755@gmail.com>
2026-06-15 11:39:35 +0530

    Remove Duplicate Folders

--- multi_vendor_ecommerce/multi_vendor_ecommerce/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from accounts import views
-urlpatterns = [
-    path('signup/',views.signup,name='signup'),
-    path('login/',views.uv_login,name='login'),
-    path('logout/', views.uv_logout, name='logout'),
-]
--- multi_vendor_ecommerce/multi_vendor_ecommerce/multi_vendor_ecommerce/multi_vendor_ecommerce/orders/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('place/', views.place_order, name='place_order'),
-    path('success/<int:order_id>/', views.order_success, name='order_success'),
-    path('order_list/', views.order_list, name='order_list'),
-    path('detail/<int:order_id>/', views.order_detail, name='order_detail'),
-]
