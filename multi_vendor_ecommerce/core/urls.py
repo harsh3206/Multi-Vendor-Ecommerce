b114418c74effa0a621bf9acc469c7c5bf314104
harsh solanki <solankiharsh7755@gmail.com>
2026-06-15 11:41:48 +0530

    duplicate files remove

--- multi_vendor_ecommerce/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from core import views
-app_name='core'
-urlpatterns = [
- path('',views.home,name="home"),
- path('dashboard/', views.admin_dashboard, name='admin_dashboard'),
- path('products/',views.product_list,name='product_list'),
- path('search/',views.search_products,name='search_products'),
-]
